9aeb047535b5c674cccda663d04d7bbf9b3307a8
lzhiming <40237873+lzhiming@users.noreply.github.com>
2025-03-26 10:25:35 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -94,7 +94,7 @@
   async function initClient() {
     await tick();
     // WebSocket 中转服务 url
-    let wsProxyUrl = 'ws://step-realtime-console-production.up.railway.app:3000';
+    let wsProxyUrl = 'wss://step-realtime-console-production.up.railway.app:3000';
 
     // 构建查询参数
     const params = new URLSearchParams();
